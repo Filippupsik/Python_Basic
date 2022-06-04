90dee599c3db3da07941b0f430330629caddd6c3
Filippupsik <76174857+Filippupsik@users.noreply.github.com>
2022-06-04 10:33:41 +0300

    Update Practice_3.py

--- Practice_3.py
+++ Practice_3.py
@@ -1,2 +1,2 @@
 #  Задаём значения для списков с помощью генератора
-print([el for el in range(20, 241) if el % 20 == 0], [el for el in range(20, 241) if el % 21 == 0])
+print([el for el in range(20, 241) if el % 20 == 0 and el for el in range(20, 241) if el % 21 == 0])
